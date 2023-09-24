4375f667f9dcb41fe0e0373da9c222f4ce220e6e
Maksim Zhdanov <89204222+Orioko@users.noreply.github.com>
2023-09-24 13:18:24 +0300

    rm-15: update import path

--- app-test.js
+++ app-test.js
@@ -1,2 +1,41 @@
-import RickService from 'app.js'
 
+// function first () {
+//     setTimeout(function () {
+//         console.log(1);
+//     }, 500);
+// }
+//
+// function second() {
+//     console.log(2);
+// }
+//
+// first();
+// second();
+
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
+//
+// function done () {
+//     console.log('Я прошел этот урок!');
+// }
+//
+// learnJS('JavaScript', done);
+
+const options = {
+    name: 'Maksim',
+    firstName: 'Zhdanov',
+    width: 1024,
+    height: 1024,
+    colors: {
+        background: 'red',
+        border: 'black'
+    }
+};
+
+console.log(options.name);
+
+delete options.name;
+
+console.log(options);
