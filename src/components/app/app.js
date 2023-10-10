9f9bf06385e19ee3f130b4f5b1c8c2a6349ee864
Maksim Zhdanov <89204222+Orioko@users.noreply.github.com>
2023-10-10 12:43:17 +0300

    rm-2 react route

---
+++ src/components/app/app.js
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+
+import './app.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Maksim Zhdanov', salary: 2800, increase: false, id: 1},
+                {name: 'Mariya Zhdanova', salary: 2500, increase: false, id: 2},
+                {name: 'Lev Zhdanov', salary: 1000, increase: false, id: 3},
+            ]
+        }
+        this.maxId = 4;
+    }
+    
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    } 
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    render () {
+        return (
+            <div className='app'>
+                <AppInfo/>
+    
+                <div className='search-panel'>
+                    <SearchPanel/>
+                    <AppFilter/>
+                    
+                </div>
+    
+                    <EmployersList 
+                        data={this.state.data}
+                        onDelete={this.deleteItem}/>
+                    <EmployersAddForm onAdd={this.addItem}/>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
-const text = 'Hello World';
+import App from './components/app/app';
 
 
-const elem = (
-  <div>
-    <h2>Текст: {text + ' Hell ' + 2023}</h2>
-    <input type='text' />
-    <button>Click</button>
-  </div>
-);
-
-// const elem = React.createElement('h2', {className: 'greting'}, 'Hello World');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem,
+  <StrictMode>
+    <App/>
+  </StrictMode>,
 );
